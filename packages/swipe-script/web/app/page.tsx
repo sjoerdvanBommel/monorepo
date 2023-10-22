4ddd991bb3262c8b20fc068355d78bb1e0937d5a
Sjoerd van Bommel <sjoerd.van.bommel@Hotmail.com>
2023-10-22 18:58:50 +0200

    chore: remove unused npmrc file

--- packages/swipe-script/web/app/page.tsx
+++ packages/swipe-script/web/app/page.tsx
@@ -1,19 +1,6 @@
-import { TRUTHY_OR_FALSY_QT_ID, prisma } from '@mr-ss/database'
 import { SwipeScript } from '../components/swipe-script'
 
-export default async function IndexPage() {
-  try {
-    const initialQuestions = await prisma.question.findMany({
-      take: 10,
-      where: {
-        difficulty_level: { in: [4, 5] },
-        question_type_id: TRUTHY_OR_FALSY_QT_ID,
-      },
-    })
-  } catch (e) {
-    console.log(e)
-  }
-
+export default function IndexPage() {
   // const initialQuestions = await getQuestions({
   //   questionTypeId: TRUTHY_OR_FALSY_QT_ID,
   //   strategyData: { strategy: 'start' },
